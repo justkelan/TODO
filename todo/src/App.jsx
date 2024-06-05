d502c7d3b46ab90bed9ec35efcfeb61a92fa5652
justkelan <your_email@example.com>
2024-06-05 16:21:45 -0400

    idk

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import ShowAllTask from './Components/ShowAllTask'
+import AddTask from './Components/AddTask'
+import UpdateTask from './Components/UpdateTask'
+import TaskNotFound from './Components/TaskNotFound'
+import Login from './Components/Login'
+import NavBar from './Components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-black  text-6xl text-center'>TO DO LIST </h1>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/view' element={<ShowAllTask />} />
+        <Route path='/add' element={<AddTask />} />
+        <Route path='/update' element={<UpdateTask />} />
+        <Route path='*' element={<TaskNotFound />} />
+      </Routes>
     </>
   )
 }
